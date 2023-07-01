7528ba6081e1263f91879ac50c7c6eaee2321ddc
Nick Stanovic <nickstanovic@gmail.com>
2023-07-01 00:40:36 -0400

    declare playwright variables

--- csharpdotnet-job-scraper/Program.cs
+++ csharpdotnet-job-scraper/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Web;
+using Microsoft.Playwright;
 
-Console.WriteLine("Hello, World!");
+string jobSearchTerm = "C#";
+string location = "Cuyahoga Falls, OH";
+int radius = 50;
+int secondsToWait = 7;
+
+string encodedJobSearchTerm = HttpUtility.UrlEncode(jobSearchTerm);
+string encodedLocation = HttpUtility.UrlEncode(location);
+
+string indeedUrl = $"https://www.indeed.com/jobs?q={encodedJobSearchTerm}&l={encodedLocation}&radius={radius}";
+
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+var context = await browser.NewContextAsync();
+
+
+var openPage = await context.NewPageAsync();
+await openPage.GotoAsync(indeedUrl);
+await openPage.WaitForTimeoutAsync(secondsToWait * 1000);
+
+await context.CloseAsync();
