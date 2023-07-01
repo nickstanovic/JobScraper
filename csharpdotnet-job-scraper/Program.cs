23a58b26ae604fccc0a402b0efbe630bb20da996
Nick Stanovic <nickstanovic@gmail.com>
2023-07-01 18:22:24 -0400

    convert html to js to console.log info

--- csharpdotnet-job-scraper/Program.cs
+++ csharpdotnet-job-scraper/Program.cs
@@ -21,3 +21,68 @@ await openPage.GotoAsync(indeedUrl);
 await openPage.WaitForTimeoutAsync(secondsToWait * 1000);
 
 await context.CloseAsync();
+
+/*
+ 
+-----
+Indeed.com retrieve all job titles, company names, locations, days ago posted, and job description in vanilla javascript
+-----
+job titles:
+
+const spanElements = document.querySelectorAll('span');
+
+spanElements.forEach((element) => {
+  const titleValue = element.title;
+  console.log(titleValue);
+});
+
+---
+company names:
+
+const companyNames = document.querySelectorAll('span.companyName');
+
+companyNames.forEach(function(element) {
+  console.log(element.innerText);
+});
+
+---
+locations:
+
+const companyLocations = document.querySelectorAll('div.companyLocation');
+
+companyLocations.forEach((element) => {
+  const textContent = element.textContent.trim();
+  console.log(textContent);
+});
+
+---
+descriptions:
+
+const parentElement = document.getElementById('jobDescriptionText');
+
+function extractTextContent(element) {
+  let text = '';
+
+// Loop through each child node of the current element
+  for (let i = 0; i < element.childNodes.length; i++) {
+    const childNode = element.childNodes[i];
+
+// Check if the child node is a text node
+    if (childNode.nodeType === Node.TEXT_NODE) {
+      // Extract the text content and append it to the result
+      text += childNode.textContent.trim();
+    } else if (childNode.nodeType === Node.ELEMENT_NODE) {
+      // Recursively call the function for child elements
+      text += extractTextContent(childNode);
+    }
+  }
+
+  return text;
+}
+
+const extractedText = extractTextContent(parentElement);
+
+console.log(extractedText)
+---
+
+*/
