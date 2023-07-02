c152adb4872bde504f07d4a1ecd3e75d67c63e05
Nick Stanovic <nickstanovic@gmail.com>
2023-07-02 18:43:01 -0400

    fix nulls and titles not logging

--- csharpdotnet-job-scraper/Program.cs
+++ csharpdotnet-job-scraper/Program.cs
@@ -1,13 +1,12 @@
-﻿using System.Web;
-using Microsoft.Playwright;
+﻿using Microsoft.Playwright;
 
 string jobSearchTerm = "C#";
 string location = "Cuyahoga Falls, OH";
 int radius = 50;
 int secondsToWait = 7;
 
-string encodedJobSearchTerm = HttpUtility.UrlEncode(jobSearchTerm);
-string encodedLocation = HttpUtility.UrlEncode(location);
+string encodedJobSearchTerm = System.Web.HttpUtility.UrlEncode(jobSearchTerm);
+string encodedLocation = System.Web.HttpUtility.UrlEncode(location);
 
 string indeedUrl = $"https://www.indeed.com/jobs?q={encodedJobSearchTerm}&l={encodedLocation}&radius={radius}";
 
@@ -18,36 +17,28 @@ var openPage = await context.NewPageAsync();
 await openPage.GotoAsync(indeedUrl);
 await openPage.WaitForTimeoutAsync(secondsToWait * 1000);
 
-// get job titles
-var titleElements = await openPage.QuerySelectorAllAsync("span");
-var titles = await Task.WhenAll(titleElements.Select(async t => await t.GetAttributeAsync("title")));
-// get company names
-var companyElements = await openPage.QuerySelectorAllAsync("span.companyName");
-var companyNames = await Task.WhenAll(companyElements.Select(async c => await c.InnerTextAsync()));
-// get locations
-var locationElements = await openPage.QuerySelectorAllAsync("div.companyLocation");
-var locations = await Task.WhenAll(locationElements.Select(async l => (await l.InnerTextAsync()).Trim()));
 
-// todo: eliminate titleElements nulls (266 nulls - lambda int titleCount = titles.Count(t => t != null); is only a band-aid)
-// todo: fix titles not logging (most likely due to nulls from Indeed poor site design)
 // todo: add click behavior to get each job description, currently only first job description is retrieved
 // todo: add pagination to scrape all listings
 
-int titleCount = titles.Count(t => t != null);
-for (int i = 0; i < titleCount; i++)
+// Get job titles
+var titleElements = await openPage.QuerySelectorAllAsync("h2.jobTitle");
+var titles = await Task.WhenAll(titleElements.Select(async t => await t.InnerTextAsync()));
+
+// Get company names
+var companyElements = await openPage.QuerySelectorAllAsync("span.companyName");
+var companyNames = await Task.WhenAll(companyElements.Select(async c => await c.InnerTextAsync()));
+
+// Get locations
+var locationElements = await openPage.QuerySelectorAllAsync("div.companyLocation");
+var locations = await Task.WhenAll(locationElements.Select(async l => (await l.InnerTextAsync()).Trim()));
+
+for (int i = 0; i < titles.Length; i++)
 {
     Console.WriteLine(titles[i]);
     Console.WriteLine(companyNames[i]);
     Console.WriteLine(locations[i]);
+    Console.WriteLine();
 }
-
-
-// get job description
-// var jobDescriptionElement = await openPage.QuerySelectorAsync("#jobDescriptionText");
-// if (jobDescriptionElement != null)
-// {
-//     var description = await jobDescriptionElement.InnerTextAsync();
-//     Console.WriteLine(description);
-// }
 
 await context.CloseAsync();
