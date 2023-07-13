13ba8cdafb72126114b25dfa6cb45aa8855cd3fb
Nick Stanovic <nickstanovic@gmail.com>
2023-07-13 05:51:08 -0400

    add db migrations to git - previously forgot

---
+++ csharpdotnet-job-scraper/Migrations/20230709233757_UpdateJobModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace indeed_scraper.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateJobModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jobs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FoundKeywords = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApplyButton = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Applied = table.Column<bool>(type: "bit", nullable: false),
+                    ScrapeTimestamp = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jobs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jobs");
+        }
+    }
+}
