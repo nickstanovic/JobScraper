7a9c8eefd4590ff2cbfd30efe8e4a703323d5051
Nick Stanovic <nickstanovic@gmail.com>
2023-07-18 16:18:16 -0400

    add more filter terms

--- JobScraper/Models/ScraperConfig.cs
+++ JobScraper/Models/ScraperConfig.cs
@@ -5,7 +5,8 @@
         public static readonly string Location = "Cuyahoga Falls, OH";
         public static readonly int Radius = 50;
         public static readonly int SecondsToWait = 10;
-        public static readonly string[] Keywords = { "c#", ".net", "sql", "blazor", "razor", "asp.net", "ef core", "entity framework", "typescript", "javascript", "angular", "git", "html", "css", "tailwind", "material", "bootstrap" };
-        public static readonly string[] AvoidJobKeywords = { "lead", "senior" };
+        public static readonly string[] Keywords = { "c#", ".net", "sql", "blazor", "razor", "asp.net", "ef core", "entity framework", "jr", "junior",
+                                                    "typescript", "javascript", "angular", "git", "html", "css", "tailwind", "material", "bootstrap" };
+        public static readonly string[] AvoidJobKeywords = { "lead", "senior", "sr", "manager" };
     }
 }
