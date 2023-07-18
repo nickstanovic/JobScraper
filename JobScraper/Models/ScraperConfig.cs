32aec66dbe69a9ba05e941c37688b5e88b0f0aad
Nick Stanovic <nickstanovic@gmail.com>
2023-07-17 20:58:31 -0400

    add ScraperConfig

---
+++ JobScraper/Models/ScraperConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace JobScraper.Models
+{
+    public static class ScraperConfig
+    {
+        public static readonly string JobSearchTerm = "C#";
+        public static readonly string Location = "Cuyahoga Falls, OH";
+        public static readonly int Radius = 50;
+        public static readonly int SecondsToWait = 10;
+        public static readonly string[] Keywords = { "c#", ".net", "sql", "blazor", "razor", "asp.net", "ef core", "entity framework", "typescript", "javascript", "angular", "git", "html", "css", "tailwind", "material", "bootstrap" };
+        public static readonly string[] AvoidJobKeywords = { "lead", "senior" };
+    }
+}
